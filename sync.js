0500bcd916e1a2bb968653b1045a2956141ad430
Rudra Sharma <robbysharma@me.com>
2023-12-21 01:24:29 -0500

    Modify embeds

--- sync.js
+++ sync.js
@@ -156,7 +156,7 @@ async function run (accessKey, budgetId, budgetEncryption, linkedAccounts, start
      if(syncErr) {
       // Return error to discord hook
       dataToSend = dataToSend.embeds[0].title.replace("%1", "Error")
-      dataToSend.embeds.fields.value = "```" + errors + "```"
+      dataToSend.embeds[0].fields[0].value = "```" + errors + "```"
     }else{
       // Return status ok to discord webhook
       dataToSend = dataToSend.embeds[0].title.replace("%1", "OK")
